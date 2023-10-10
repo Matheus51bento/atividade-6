842274603da7f4fbee723fe712ebb8f9653dfee1
Matheus Bento <matheus51bento@gmail.com>
2023-10-10 20:38:34 -0300

    atv terminada

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from core.models import Disco
+from core.models import Disco, Artista, Selo
+
 
 admin.site.register(Disco)
+admin.site.register(Artista)
+admin.site.register(Selo)
--- core/models.py
+++ core/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
+
+class Selo(models.Model):
+    texto = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.texto
+
+
 class Disco(models.Model):
     nome = models.CharField(max_length=200)
     descricao = models.CharField(max_length=500)
-    selo_fonografico = models.CharField(max_length=100)
+    selo_fonografico = models.ForeignKey(Selo, null=True, blank=True, on_delete=models.CASCADE)
     ano = models.IntegerField()
     pais = models.CharField(max_length=80)
     genero = models.CharField(max_length=80)
     quantidade = models.BigIntegerField()
+
+    def __str__(self):
+        return self.nome
+
+class Artista(models.Model):
+    nome = models.CharField(max_length=80)
+    discos = models.ManyToManyField(Disco)
 
     def __str__(self):
         return self.nome
